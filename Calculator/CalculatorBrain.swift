7dc244e73a2aa4e01ccc9ecf06fc9056579289f2
Theo Skolnik <theodoraskolnik@gmail.com>
2016-06-13 12:50:13 -0500

    first commit

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,78 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Theodora Skolnik on 6/12/16.
+//  Copyright © 2016 Theodora Skolnik. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain
+{
+    private var accumulator = 0.0
+    
+    func setOperand(operand: Double) {
+        accumulator = operand
+    }
+    
+     private var operations: Dictionary<String, Operation> = [
+        "π" : Operation.Constant(M_PI),
+        "e" : Operation.Constant(M_E),
+        "√" : Operation.UnaryOperation(sqrt),
+        "cos" : Operation.UnaryOperation(cos),
+        "sin" : Operation.UnaryOperation(sin),
+        "tan" : Operation.UnaryOperation(tan),
+        "×" : Operation.BinaryOperation({ $0 * $1 }),
+        "÷" : Operation.BinaryOperation({ $0 / $1 }),
+        "+" : Operation.BinaryOperation({ $0 + $1 }),
+        "−" : Operation.BinaryOperation({ $0 - $1 }),
+        "x²" : Operation.UnaryOperation({ $0 * $0 }),
+        "xʸ" : Operation.BinaryOperation({ pow($0, $1) }),
+        "1/x" : Operation.UnaryOperation({ 1 / $0 }),
+        "=" : Operation.Equals
+    ]
+    
+    private enum Operation {
+        case Constant(Double)
+        case UnaryOperation((Double) -> Double)
+        case BinaryOperation((Double, Double) -> Double)
+        case Equals
+    }
+    
+    func performOperation(symbol: String) {
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value):
+                accumulator = value
+            case .UnaryOperation(let function):
+                accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                executePendingBinaryOperation()
+                pending = PendingBinaryOperationInfo(binaryFunction: function, firstOperand: accumulator)
+            case .Equals :
+                executePendingBinaryOperation()
+            }
+        }
+    }
+    
+    private func executePendingBinaryOperation() {
+        if pending != nil {
+            accumulator = pending!.binaryFunction(pending!.firstOperand, accumulator)
+            pending = nil
+        }
+    }
+    
+    private var pending: PendingBinaryOperationInfo?
+    
+    private struct PendingBinaryOperationInfo {
+        var binaryFunction: (Double, Double) -> Double
+        var firstOperand: Double
+    }
+    
+    var result: Double {
+        get {
+            return accumulator
+        }
+    }
+}
---
+++ Calculator/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Theodora Skolnik on 6/12/16.
+//  Copyright © 2016 Theodora Skolnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    private var brain = CalculatorBrain()
+    
+    @IBAction private func performOperation(sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        displayValue = brain.result
+    }
+    
+    @IBAction func addDecimalPoint(sender: UIButton) {
+        let period = sender.currentTitle!
+        if userIsInTheMiddleOfTyping && !display.text!.containsString(".") {
+            display.text = display.text! + period
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+}
